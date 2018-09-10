0083b810fa10a3e16d6b8cd2c4f75e93c4a92c23
Howie Zhu <hwie.zhu@gmail.com>
2018-09-09 20:17:10 -0400

    added mapbox, pins, functional zipcode search, api

---
+++ src/NewForm/index.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+export default class NewForm extends Component{
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.submitButton(this.props.task)
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input 
+          onChange={this.props.handleUpdateInput}
+          value={this.props.zipcode}
+          type="text"
+          className='form'
+          maxLength='5'
+          >
+          </input>
+          <button 
+          className='submitButton'
+          >
+            SUBMIT
+          </button>
+        </form>
+      </div>
+    )
+  }
+}
